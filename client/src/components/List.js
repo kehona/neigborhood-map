1d6ba9af85d1370c78184278d824015ba8864367
Kehinde Onadipe <kenodipe@gmail.com>
2018-10-22 21:23:28 -0500

    add css style to location list

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 
 class List extends Component {
-    render() {
-        return (<div id="list-section">
-            <div id="location">
-                <h5> Wifi locations in your neighorhood</h5>
-                <ol>
-                    {this.props.locations.map(location => (
-                        <p>{location.venue.name}</p>
-                    ))}
-                </ol>
-            </div>
-        </div>)
-    }
+  render() {
+    return (
+      <div id="list-section">
+        <div id="location">
+          <h5> Wifi locations in your neighorhood</h5>
+          <input type="text" placeholder="Search by location" />
+          <ol>
+            {this.props.locations.map(location => (
+              <li>
+                <div className="location-details">
+                  <p class="title">{location.venue.name}</p>
+                  <p class="info">Address: {location.venue.location.address}</p>
+                  <p class="info">Type: {location.venue.categories[0].name}</p>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
 export default List;
