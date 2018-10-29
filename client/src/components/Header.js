31f360291715ab43387f8917e3ab1357e299653e
Kehinde Onadipe <kenodipe@gmail.com>
2018-10-29 09:46:59 -0500

    add style to list to make it responsive

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 
 class Header extends Component {
+  handleClick() {
+    let div = document.getElementById("list-section");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+    console.log(div);
+  }
   render() {
     return (
       <header id="pageHeader" className="row">
         <h1 id="title">Ken's Cafe</h1>
+        <a href="#" onClick={this.handleClick}>
+          <i className="fa fa-bars" aria-hidden="true" />
+        </a>
       </header>
     );
   }
